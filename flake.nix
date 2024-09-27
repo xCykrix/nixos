c7dcae720dbe53171341975c4163de4d322b2400
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-09-27 10:20:35 -0400

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Operator Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
